93d77d0594271426ee7d9271bb198630926c1b34
aghassel <aghassel@gmail.com>
2023-04-11 02:24:24 -0400

    finished classificiation

--- feature_classification.py
+++ feature_classification.py
@@ -42,7 +42,8 @@ def segment_data(data, window_size=5, sample_rate=100):
 
 def extract_features(segment):
     features = []
-    for column in segment.columns[:-1]:  # Exclude the 'Activity' column
+    featured_columns = ['Absolute acceleration (m/s^2)']  # only keep the absolute acceleration column
+    for column in featured_columns:  # Exclude the 'Activity' column
         column_data = segment[column]
         mean = column_data.mean()
         std = column_data.std()
@@ -50,9 +51,9 @@ def extract_features(segment):
         maximum = column_data.max()
         skewness = column_data.skew()
         kurtosis = column_data.kurtosis()
-        energy = np.sum(column_data ** 2)
-        features.extend([mean, std, minimum, maximum, skewness, kurtosis, energy])
+        features.extend([mean, std, minimum, maximum, skewness, kurtosis])
     return features
+
 
 # Load the data and preprocess
 data_files = {
@@ -71,54 +72,51 @@ data_files = {
 activity_codes = {"jumping": 0, "walking": 1}
 
 labeled_data = load_and_label_data(data_files)
-preprocessed_data = [remove_five_seconds_rolling(df, 100) for df in labeled_data]
+preprocessed_data = [remove_five_seconds_rolling(df, 20) for df in labeled_data]
 
 segmented_data = []
 for data in preprocessed_data:
     segmented_data.extend(segment_data(data))
 
-# Extract features and labels
-feature_matrix = []
+# Prepare data and labels
+data_matrix = []
 label_vector = []
 
 for segment in segmented_data:
-    features = extract_features(segment)
+    data_matrix.append(segment)
     label = segment.iloc[0, -1]
-    feature_matrix.append(features)
     label_vector.append(label)
 
-feature_matrix = np.array(feature_matrix)
-label_vector = np.array(label_vector)
-
 # Split the data into training and testing sets
-X_train, X_test, Y_train, Y_test = train_test_split(feature_matrix, label_vector, test_size=0.1, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(data_matrix, label_vector, test_size=0.1, random_state=42)
+
+# Extract features for train and test sets
+X_train_features = [extract_features(segment) for segment in X_train]
+X_test_features = [extract_features(segment) for segment in X_test]
+
+X_train_features = np.array(X_train_features)
+X_test_features = np.array(X_test_features)
 
 # define Standard Scaler to normalize inputs
 scaler = StandardScaler()
 l_reg = LogisticRegression(max_iter=10000)
 pca = PCA(n_components=2)
 
-# defining classifier and pupeline
+# defining classifier and pipeline
 l_reg = LogisticRegression(max_iter=10000)
 pca_pipe = make_pipeline(StandardScaler(), pca)
 clf = make_pipeline(l_reg)
 
-X_train_pca = pca_pipe.fit_transform(X_train)
-X_test_pca = pca_pipe.fit_transform(X_test)
+X_train_pca = pca_pipe.fit_transform(X_train_features)
+X_test_pca = pca_pipe.fit_transform(X_test_features)
 
 clf.fit(X_train_pca, Y_train)
 
-# train
-# pca_pipe.fit(X_train, Y_train)
-
 y_pred_clf = clf.predict(X_test_pca)
-#y_clf_prob = clf.predict_proba(X_test)
 print("y_pred_clf is: ", y_pred_clf)
-#print("y_clf_prob is: ", y_clf_prob)
 
 acc = accuracy_score(Y_test, y_pred_clf)
 print("Accuracy: ", acc)
       
 recall = recall_score(Y_test, y_pred_clf)
 print("recall is : ", recall)
-
